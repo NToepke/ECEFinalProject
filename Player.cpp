4264ff94b825cee15b62b691faf4ed2f80a7edcd
Nathan Toepke <nathantoepke@gmail.com>
2023-05-01 16:30:15 -0500

    Create incrementInteractionMap function
    increments a value given a key for the player map, this is used to progress through the story.

--- Player.cpp
+++ Player.cpp
@@ -14,7 +14,19 @@ static vector<InteractObject>* Player::getOptions()
     return currentInteractions;
 }
 
-static map<std::string,int>* Player::getMap()
+static map<std::string,int>* Player::getInteractionMap()
 {
     return progressionMap;
+}
+
+static int Player::incrementInteractionMap(std::string interactName)
+{
+    try {
+        progressionMap.at(interactName)++;
+    }
+    catch (const std::out_of_range& e){
+        std::cerr << "Error: " << e.what() << std::endl;
+        return -1;
+    }
+    return progressionMap.at(interactName);
 }
--- Player.h
+++ Player.h
@@ -9,7 +9,11 @@
 class Player {
     private:
         static Player* instance;
-        static std::map<std::string interactName,int progressionKey> progressionMap;
+        /*
+        This Map has a key of a string, which is the InteractObject string, and an Int value,
+        The int value is used to track progression through the object on the player side.
+        */
+        static std::map<std::string,int> progressionMap; 
         static std::vector<InteractObject> currentInteractions;
         Player(){};
 
@@ -17,7 +21,8 @@ class Player {
     public:
         static Player* getInstance();
         static vector<InteractObject>* getOptions();
-        static map<std::string,int>* getMap();
+        static map<std::string,int>* getInteractionMap();
+        static int incrementInteractionMap(std::string interactName);
 };
 
 
