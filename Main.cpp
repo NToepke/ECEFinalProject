5455a1157c9a287240532bc7b80b123ba9724750
Nathan Toepke <nathantoepke@gmail.com>
2023-05-01 16:31:10 -0500

    Improve runMenu()
    worked on gameplay loop in main, added user input to the menu and some error trapping for map progression

--- Main.cpp
+++ Main.cpp
@@ -10,19 +10,43 @@ int main()
     Player* player = Player.getInstance(); //Create the player
     //Intro
     //RunMenu loop
-    int result = runMenu();
+    std::string chosenInteraction = "";
+    do
+    {
+        chosenInteraction = runMenu(player);
+    }
+    while(chosenInteraction.compare("-1") != 0);
+    //exit menu loop when game tells you to.
+    //Thanks for playing message
     return EXIT_SUCCESS;
 }
 
-int runMenu(Player* player)
+std::string runMenu(Player* player)
 {
+    //pull player variables into local scope for easier code readability
     vector<interactObject>* currentOptions = player->getOptions();
-    map<std::string,int>* progressionMap = player->getMap();
+    map<std::string,int>* progressionMap = player->getInteractionMap();
+    //variable to hold user selection
+    int userInput = -1;
     for(int i = 1; i <= currentOptions.size(); i++)
     {
+        //get the string from each InteractObject to print for user selection
         int currentProgression = progressionMap[currentOptions[i-1]->name];
         std::string printDesc = currentOptions[i-1].descriptions[currentProgression];
-
+        //print that string for user to see.
         std::cout << i <<". "<< printDesc << std::endl;
+
     }
+    std::cin >> userInput;
+
+    //Run story portion of current interaction
+
+    int checkFail = player->incrementInteractionMap(currentOptions[userInput-1]->name);
+    if(checkFail == -1)
+    {
+        std::cerr << "Increment failed, look above for error from method." << std::endl;
+        //game is broken, exit out
+        return "-1";
+    }
+
 }
