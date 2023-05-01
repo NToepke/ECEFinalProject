dc5baa1ba9c265356cd9876d859e5af1e4fb0511
Nathan Toepke <nathantoepke@gmail.com>
2023-05-01 10:28:55 -0500

    Add include guards

--- InteractObjectFactory.h
+++ InteractObjectFactory.h
@@ -1,6 +1,9 @@
+#ifndef FACTORY_H
+#define FACTORY_H
 #include "InteractObject.h"
 
 class InteractObjectFactory {
     public:
         static InteractObject* getInteractObject(enum objectType);
 }
+#endif
