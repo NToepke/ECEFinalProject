07187cb0703ff75a9ae033966b405702b2caac77
Nathan Toepke <nathantoepke@gmail.com>
2023-04-30 21:49:38 -0500

    Initial brainstorming for the Main.cpp
    This code will change but its an initial start for how the game will begin running.

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include <Vector.h>
+#include "Player.h"
+#include "Npc.h"
+#include "Quest.h"
+#include "Furniture.h"
+
+/*
+*/
+
+int main()
+{
+    Player* player = Player.getInstance(); //Create the player
+    //Intro
+    //RunMenu loop
+    int result = runMenu()
+    return EXIT_SUCCESS;
+}
+
+int runMenu(Player* player)
+{
+    for(int i = 1; i < ; i++)
+    {
+        std::cout << i <<". "<<choices[i-1].description << std::endl;
+    }
+}
