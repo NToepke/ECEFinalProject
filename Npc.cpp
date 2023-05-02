ce8bccee92c9809a1d3850b1f69139007c3969b0
NToepke <57374771+NToepke@users.noreply.github.com>
2023-05-02 15:08:35 -0500

    Finish constructors and add setDescriptions method

--- Npc.cpp
+++ Npc.cpp
@@ -2,14 +2,19 @@
 
 Npc::Npc()
 {
-
+    this->name = "Unnamed";
 }
 Npc::Npc(std::string name)
 {
+    this->name = name;
+}
 
+void Npc::setDescriptions(std::vector<std::string> descriptions)
+{
+    this->descriptions = descriptions;
 }
 
 std::string Npc::giveDescription(int currentProgression)
 {
-    
+    return "";
 }
