8fa37bdf1cd2375985aa81b37f55ec546dc8bcf7
NToepke <57374771+NToepke@users.noreply.github.com>
2023-05-02 15:20:13 -0500

    Add BasicStory(), bug fixes
    BasicStory() is a basic room for testing code.

--- Main.cpp
+++ Main.cpp
@@ -3,52 +3,67 @@
 #include "Npc.h"
 #include "Furniture.h"
 #include "InteractObject.h"
-
-/*
-*/
-
-int main()
-{
-    Player* player = Player::getInstance(); //Create the player
-    //Intro
-    //RunMenu loop
-    std::string chosenInteraction = "";
-    do
-    {
-        chosenInteraction = runMenu(player);
-    }
-    while(chosenInteraction.compare("-1") != 0);
-    //exit menu loop when game tells you to.
-    //Thanks for playing message
-    return EXIT_SUCCESS;
-}
+#include "InteractObjectFactory.h"
 
 std::string runMenu(Player* player)
 {
     //pull player variables into local scope for easier code readability
-    std::vector<InteractObject>* currentOptions = player->getOptions();
+    std::vector<InteractObject*>* currentOptions = player->getInteractions();
     std::map<std::string,int>* progressionMap = player->getInteractionMap();
     //variable to hold user selection
     int userInput = -1;
-    for(int i = 1; i <= currentOptions->size(); i++)
+    for(unsigned i = 1; i <= currentOptions->size(); i++)
     {
         //get the string from each InteractObject to print for user selection
         //print the name for user to see.
-        std::cout << i <<". " << currentOptions->at(i-1).name << std::endl;
+        std::cout << i <<". " << currentOptions->at(i-1)->name << std::endl;
     }
     std::cin >> userInput;
 
-    int currentProgression = progressionMap->at(currentOptions->at(userInput-1).name);
-    std::string printDesc = currentOptions->at(userInput-1).descriptions[currentProgression];
-    //descriptions[currentProgression];
+    int currentProgression = progressionMap->at(currentOptions->at(userInput-1)->name);
+    std::string printDesc = currentOptions->at(userInput-1)->descriptions[currentProgression];
     //Run story portion of current interaction
-
-    int checkFail = player->incrementInteractionMap(currentOptions->at(userInput-1).name);
+    int checkFail = player->incrementInteractionMap(currentOptions->at(userInput-1)->name);
     if(checkFail == -1)
     {
         std::cerr << "Increment failed, look above for error from method." << std::endl;
         //game is broken, exit out
         return "-1";
     }
-    return "";
+    return printDesc;
+}
+
+void basicStory(Player* player, InteractObjectFactory factory)
+{
+    std::vector<InteractObject*> newStory;
+    newStory.push_back(factory.getInteractObject("Furniture","Bookshelf"));
+    newStory.push_back(factory.getInteractObject("Npc","Jeff"));
+    newStory.push_back(factory.getInteractObject("Furniture","Desk"));
+
+    newStory.at(0)->setDescriptions({"Its a bookshelf covered in books.","Story 2","Story 3"});
+    newStory.at(1)->setDescriptions({"Hi!","Story 2","Story 3"});
+    newStory.at(2)->setDescriptions({"It's a small desk with no drawers.","Story 2","Story 3"});
+
+    player->addInteraction(newStory[0]);
+    player->addInteraction(newStory[1]);
+    player->addInteraction(newStory[2]);
+}
+
+int main()
+{
+    Player* player = Player::getInstance(); //Create the player
+    InteractObjectFactory factory;
+    //Intro
+    basicStory(player,factory);
+    //RunMenu loop
+    std::string chosenInteraction = "";
+    do
+    {
+        chosenInteraction = runMenu(player);
+        std::cout << chosenInteraction <<std::endl;
+    }
+    while(chosenInteraction.compare("-1") != 0);
+    //exit menu loop when game tells you to.
+    //Thanks for playing message
+    return EXIT_SUCCESS;
 }
