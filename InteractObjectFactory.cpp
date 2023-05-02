1e1f1ad21236d9da2e27b19957b6a8e340fc2535
Nathan Toepke <nathantoepke@gmail.com>
2023-05-02 00:45:54 -0500

    Update InteractObjectFactory.cpp
    begin the code for creating objects of the InteractObject superclasses.

---
+++ InteractObjectFactory.cpp
@@ -0,0 +1,20 @@
+#include "InteractObjectFactory.h"
+#include "Furniture.h"
+#include "Npc.h"
+
+InteractObject* InteractObjectFactory::getInteractObject(std::string objectType, std::string name)
+{
+    if(objectType.compare("Furniture")==0)
+    {
+        return new Furniture(name);
+    }
+    else if(objectType.compare("Npc")==0)
+    {
+        return new Npc(name);
+    }
+    else
+    {
+        //Error
+        return nullptr;
+    }
+}
