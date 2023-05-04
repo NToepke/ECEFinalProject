6ab44752d159ead3a2c58af377e328d86f99de4b
NToepke <57374771+NToepke@users.noreply.github.com>
2023-05-04 01:53:41 -0500

    Created New Test file for Furniture
    So to generate the build file, run command: cmake -S . -B build
    Then run the command actually make a build of the tests: cmake --build build
    Finally, run: cd build & ctest
    
    This should allow a developer to use the gtests.

--- FurnitureTests.cc
+++ FurnitureTests.cc
@@ -1,12 +1,16 @@
 #include <gtest/gtest.h>
+#include "Furniture.h"
 //I cannot for the life of me figure out how to add this to my VSCode
 //So it will constantly be squiggly red line for me I guess.
 
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest2, BasicAssertions2) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "hello");
+  //Furniture temp = Furniture("Jim");
+  EXPECT_EQ(5,5);
+
+  //EXPECT_NE(temp.name,Furniture().name);
+  //EXPECT_NE(temp.name,Furniture("Jeff").name);
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
 }
