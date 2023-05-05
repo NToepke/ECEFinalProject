1eede268758bd4cb471cc15c8334d961ae06d3bc
NToepke <57374771+NToepke@users.noreply.github.com>
2023-05-05 08:05:50 -0500

    Write testcases for Furniture

--- FurnitureTests.cc
+++ FurnitureTests.cc
@@ -5,12 +5,45 @@
 
 
 // Demonstrate some basic assertions.
-TEST(HelloTest2, BasicAssertions2) {
-  // Expect two strings not to be equal.
-  //Furniture temp = Furniture("Jim");
-  EXPECT_EQ(5,5);
+TEST(FurnitureConstructor, ThreeParams) {
+  Furniture* tempA = new Furniture("tempA",{"The first Story","The second story!", "The THIRD story?"},{"NONE","NONE","NONE"});
+  Furniture* tempB = new Furniture("tempB",{"B story One","B second story!", "The THIRD story?"},{"NONE","NONE","NONE"});
+  EXPECT_NE(tempA->name,tempB->name);
+  EXPECT_EQ(tempA->descriptions[2],tempB->descriptions[2]);
+  EXPECT_EQ(tempA->increments[2],tempA->increments[2]);
+}
 
-  //EXPECT_NE(temp.name,Furniture().name);
-  //EXPECT_NE(temp.name,Furniture("Jeff").name);
-  // Expect equality.
+TEST(FurnitureConstructor, TwoParams) {
+  Furniture tempA = Furniture("tempA",{"The first Story","The second story!", "The THIRD story?"});
+  Furniture tempB = Furniture("tempB",{"B story One","B second story!", "The THIRD story?"});
+  EXPECT_NE(tempA.name,tempB.name);
+  EXPECT_EQ(tempA.descriptions[2],tempB.descriptions[2]);
+}
+
+TEST(FurnitureConstructor, OneParam) {
+  Furniture tempA = Furniture("tempA");
+  Furniture tempB = Furniture("tempB");
+  EXPECT_NE(tempA.name,tempB.name);
+}
+
+TEST(FurnitureConstructor, NoParam) {
+  Furniture tempA = Furniture();
+  Furniture tempB = Furniture();
+  EXPECT_EQ(tempA.name,tempB.name);
+}
+
+TEST(FurnitureMethods, goodDescription) {
+  Furniture tempA = Furniture("tempA",{"The first Story","The second story!", "The THIRD story?"},{"NONE","NONE","JEFF"});
+  EXPECT_NE(tempA.name,"tempA");
+  EXPECT_EQ(tempA.descriptions[2],"The THIRD story?");
+  EXPECT_EQ(tempA.increments[2],"NONE");
+
+  tempA.setDescriptions({"Yes","No","Maybe"});
+  EXPECT_EQ(tempA.descriptions[0],"Yes");
+  EXPECT_EQ(tempA.descriptions[1],"No");
+  EXPECT_EQ(tempA.descriptions[2],"Maybe");
+
+  EXPECT_EQ(tempA.giveDescription(0),"Yes");
+  EXPECT_EQ(tempA.giveDescription(1),"No");
+  EXPECT_EQ(tempA.giveDescription(2),"Maybe");
 }
